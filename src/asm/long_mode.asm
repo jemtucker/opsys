e9187515b655e69a9ac1231810d5ee9ce4e1ff67
Jem Tucker <jem.tucker@gmail.com>
2016-06-22 22:12:38 +0100

    Initial vga_buffer module to handle output to the console

--- src/asm/long_mode.asm
+++ src/asm/long_mode.asm
@@ -3,9 +3,19 @@ global long_mode_start
 ; Finally into 64 bit mode :)
 bits 64
 long_mode_start:
-
+	
+	; Call the kernel_main function 
     extern kernel_main
     call kernel_main
+
+    ; kernel_main returned, print `OS returned!`
+    mov rax, 0x4f724f204f534f4f
+    mov [0xb8000], rax
+    mov rax, 0x4f724f754f744f65
+    mov [0xb8008], rax
+    mov rax, 0x4f214f644f654f6e
+    mov [0xb8010], rax
+    hlt
 
     cli
     hlt
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,24 @@
 #![feature(lang_items)]
+#![feature(const_fn)]
+#![feature(unique)]
+
 #![no_std]
+
+extern crate rlibc;
+extern crate spin;
+
+#[macro_use]
+mod vga_buffer;
 
 // Main entry point, need no_mangle so we can call from assembly
 // Extern to abide with C calling convention
 #[no_mangle]
 pub extern fn kernel_main() {
-	unsafe {
-        let vga = 0xb8000 as *mut u64;
+	vga_buffer::clear_screen();
 
-        *vga = 0x2f592f412f4b2f4f;
-    };
+	kprintln!("Hello World!");
+
+	loop {}
 }
 
 // For stack-unwinding, not supported currently
