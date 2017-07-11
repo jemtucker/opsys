29ebce577a0e66e4267dc1afa5c4e559c372b775
Jem Tucker <jem.tucker@gmail.com>
2017-07-11 09:07:21 +0100

    Refactor allocator to support new rust allocator_api

--- libs/alloc_opsys/src/allocator.rs
+++ libs/alloc_opsys/src/allocator.rs
@@ -1,6 +1,7 @@
-use ::block::Block;
+use block::Block;
 
-use ::core::mem::size_of;
+use core::mem::size_of;
+use alloc::allocator::{Alloc, Layout, AllocErr};
 
 //const MIN_ALLOCATION: usize = 1;
 //const MIN_BLOCK_SIZE: usize = size_of::<Block>() + MIN_ALLOCATION;
@@ -15,11 +16,10 @@ const MIN_BLOCK_SIZE: usize = 50;
 //
 
 pub struct Allocator {
-    block_head: *mut Block
+    block_head: *mut Block,
 }
 
 impl Allocator {
-
     // Create a new Allocator for a given heap. The heap must be at-least the size of a block
     // header.
     pub fn new(heap: &mut [u8], size: usize) -> Allocator {
@@ -33,20 +33,46 @@ impl Allocator {
             b
         };
 
-        Allocator {
-            block_head: block
-        }
+        Allocator { block_head: block }
     }
 
-    // Allocate 'size' byes
-    pub fn alloc(&mut self, size: usize, _: usize) -> Option<*mut u8> {
+    unsafe fn next_fit(
+        &mut self,
+        size: usize,
+        current: *mut Block,
+    ) -> Result<*mut Block, AllocErr> {
+        let current_ref = match current.as_ref() {
+            Some(cur) => Ok(cur),
+            None => Err(AllocErr::Unsupported { details: "NULL block ptr" }),
+        }?;
+
+        if current_ref.free && current_ref.size >= size {
+            Ok(current)
+        } else {
+            let block = match current_ref.next {
+                Some(next) => Ok(next),
+                None => {
+                    // Out of memory.
+                    let layout = Layout::from_size_align_unchecked(size, 0);
+                    Err(AllocErr::Exhausted { request: layout })
+                }
+            }?;
+
+            self.next_fit(size, block)
+        }
+    }
+}
+
+unsafe impl Alloc for Allocator {
+    unsafe fn alloc(&mut self, layout: Layout) -> Result<*mut u8, AllocErr> {
+        let size = layout.size();
 
         // TODO implement the alignment side of things
 
         // Find the next fitting block
         let block_head = self.block_head;
-        let block_ptr = self.next_fit(size, block_head);
-        let mut block = unsafe { block_ptr.as_mut().expect("Null Block Pointer") };
+        let block_ptr = self.next_fit(size, block_head)?;
+        let mut block = block_ptr.as_mut().expect("Null Block Pointer");
 
         // Found a block. We now need to see how big it is. If after allocation it is going to
         // leave unused memory larger than MIN_BLOCK_SIZE then we chunk it up and create a new
@@ -62,13 +88,11 @@ impl Allocator {
             // Get a pointer to the next block and set it to point to whatever the original 'next'
             // was. This is because we are slotting this block in between the allocated block
             // and its neighbour.
-            let mut next_block = unsafe { block.next_ptr() };
-            unsafe {
-                (*next_block).size = next_block_size;
-                (*next_block).prev = Some(block as *mut Block);
-                (*next_block).next = block.next;
-                (*next_block).free = true;
-            }
+            let mut next_block = block.next_ptr();
+            (*next_block).size = next_block_size;
+            (*next_block).prev = Some(block as *mut Block);
+            (*next_block).next = block.next;
+            (*next_block).free = true;
 
             // Finally set the allocated block to point to our new block and complete the chain.
             block.next = Some(next_block);
@@ -76,59 +100,44 @@ impl Allocator {
 
         // Finally we mark the allocated block as used and return the data_pointer to the caller
         block.free = false;
-        let alloc_pointer = unsafe { block.data_pointer() };
-        Some(alloc_pointer)
+        let alloc_pointer = block.data_pointer();
+        Ok(alloc_pointer)
     }
 
-    pub fn dealloc(&mut self, ptr: *mut u8, size: usize, _: usize) {
-        let mut block = unsafe { get_block_ptr(ptr) };
+    unsafe fn dealloc(&mut self, ptr: *mut u8, layout: Layout) {
+        let mut block = get_block_ptr(ptr);
 
         // This is only for the unit tests...
-        debug_assert!(block.size == size);
+        debug_assert!(block.size == layout.size());
 
         // Set the block as free
         block.free = true;
 
         // TODO Merge neighbours.
     }
-
-    pub fn usable_size(&mut self, size: usize, _: usize) -> usize {
-        // TODO Implement this properly...
-        size
-    }
-
-    pub fn realloc(&mut self, ptr: *mut u8, size: usize, new_size: usize,
-        align: usize) -> Option<*mut u8> {
-        // TODO We should check here wether the new size is smaller, if so maybe just return the
-        // original pointer?
-
-        self.dealloc(ptr, size, align);
-
-        self.alloc(new_size, align)
-    }
-
-    pub fn realloc_inplace(&mut self, _: *mut u8, size: usize,
-        _: usize, _: usize) -> usize {
-        // TODO Implement this properly...
-        size
-    }
-
-    fn next_fit(&mut self, size: usize, current: *mut Block) -> *mut Block {
-        let current_ref = unsafe { current.as_ref().expect("Null Block Pointer") };
-
-        if current_ref.free && current_ref.size >= size {
-            current
-        } else {
-            let block = match current_ref.next {
-                Some(next) => next,
-                None => panic!("Out Of Memory"),
-            };
-
-            self.next_fit(size, block)
-        }
-    }
-
 }
+
+
+// pub fn usable_size(&mut self, size: usize, _: usize) -> usize {
+//     // TODO Implement this properly...
+//     size
+// }
+//
+// pub fn realloc(&mut self, ptr: *mut u8, size: usize, new_size: usize,
+//     align: usize) -> Option<*mut u8> {
+//     // TODO We should check here wether the new size is smaller, if so maybe just return the
+//     // original pointer?
+//
+//     self.dealloc(ptr, size, align);
+//
+//     self.alloc(new_size, align)
+// }
+//
+// pub fn realloc_inplace(&mut self, _: *mut u8, size: usize,
+//     _: usize, _: usize) -> usize {
+//     // TODO Implement this properly...
+//     size
+// }
 
 // Get a pointer to the block that is encapsulating a given u8 pointer
 unsafe fn get_block_ptr<'a>(ptr: *mut u8) -> &'a mut Block {
--- libs/alloc_opsys/src/test.rs
+++ libs/alloc_opsys/src/test.rs
@@ -1,5 +1,7 @@
 use super::allocator::Allocator;
 
+use alloc::allocator::{Alloc, Layout};
+
 // Bunch of different heaps as the tests run concurrently
 const HEAP_SIZE: usize = 1000;
 
@@ -9,7 +11,12 @@ const HEAP_SIZE: usize = 1000;
 fn alloc_0() {
     let mut heap = [0; HEAP_SIZE];
     let mut allocator = Allocator::new(&mut heap, HEAP_SIZE);
-    let _ = allocator.alloc(26, 0);
+
+    unsafe {
+        let layout = Layout::from_size_align_unchecked(26, 0);
+        let result = allocator.alloc(layout);
+        assert!(result.is_ok());
+    }
 }
 
 #[test]
@@ -17,33 +24,48 @@ fn alloc_1() {
     let mut heap = [0; HEAP_SIZE];
     let mut allocator = Allocator::new(&mut heap, HEAP_SIZE);
 
-    // Allocate the entire heap
-    for _ in 0..10 {
-        let _ = allocator.alloc(100 - ::core::mem::size_of::<::block::Block>(), 0);
+    unsafe {
+        // Allocate the entire heap
+        for _ in 0..10 {
+            let size = 100 - ::core::mem::size_of::<::block::Block>();
+            let layout = Layout::from_size_align_unchecked(size, 0);
+            let result = allocator.alloc(layout);
+            assert!(result.is_ok());
+        }
     }
 }
 
 #[test]
-#[should_panic(expected = "Out Of Memory")]
-fn panics_on_oom_0() {
+fn returns_err_on_oom_01() {
     let mut heap = [0; HEAP_SIZE];
     let mut allocator = Allocator::new(&mut heap, HEAP_SIZE);
-    let _ = allocator.alloc(1001, 0);
+
+    unsafe {
+        let layout = Layout::from_size_align_unchecked(HEAP_SIZE + 1, 0);
+        let result = allocator.alloc(layout);
+        assert!(result.is_err());
+    }
 }
 
 #[test]
-#[should_panic(expected = "Out Of Memory")]
-fn panics_on_oom_1() {
+fn returns_err_on_oom_02() {
     let mut heap = [0; HEAP_SIZE];
     let mut allocator = Allocator::new(&mut heap, HEAP_SIZE);
 
-    // Allocate the entire heap
-    for _ in 0..10 {
-        let _ = allocator.alloc(100 - ::core::mem::size_of::<::block::Block>(), 0);
-    }
+    unsafe {
+        // Allocate the entire heap
+        for _ in 0..10 {
+            let size = 100 - ::core::mem::size_of::<::block::Block>();
+            let layout = Layout::from_size_align_unchecked(size, 0);
+            let result = allocator.alloc(layout);
+            assert!(result.is_ok());
+        }
 
-    // And OOM it...
-    let _ = allocator.alloc(1, 0);
+        // And OOM it...
+        let layout = Layout::from_size_align_unchecked(1, 0);
+        let result = allocator.alloc(layout);
+        assert!(result.is_err());
+    }
 }
 
 // Dealloc tests
@@ -55,11 +77,15 @@ fn dealloc_0() {
 
     let size = 100 - ::core::mem::size_of::<::block::Block>();
 
-    // Allocate some memory
-    let p1 = allocator.alloc(size, 0);
+    unsafe {
+        // Allocate some memory
+        let layout_a = Layout::from_size_align_unchecked(size, 0);
+        let p1 = allocator.alloc(layout_a);
 
-    // Deallocate it...
-    allocator.dealloc(p1.unwrap(), size, 0);
+        // Deallocate it...
+        let layout_d = Layout::from_size_align_unchecked(size, 0);
+        allocator.dealloc(p1.unwrap(), layout_d);
+    }
 }
 
 #[test]
@@ -69,70 +95,80 @@ fn dealloc_1() {
 
     let size = 500 - ::core::mem::size_of::<::block::Block>();
 
-    // Allocate the entire heap in two allocs
-    let p1 = allocator.alloc(size, 0);
-    let _ = allocator.alloc(size, 0);
+    unsafe {
+        // Allocate the entire heap in two allocs
+        let layout_a1 = Layout::from_size_align_unchecked(size, 0);
+        let p1 = allocator.alloc(layout_a1);
+        assert!(p1.is_ok());
 
-    // Deallocate the first
-    allocator.dealloc(p1.unwrap(), size, 0);
+        let layout_a2 = Layout::from_size_align_unchecked(size, 0);
+        let p2 = allocator.alloc(layout_a2);
+        assert!(p2.is_ok());
 
-    // Try to reallocate the same memory freed from p1
-    let _ = allocator.alloc(size, 0);
-}
+        // Deallocate the first
+        let layout_d = Layout::from_size_align_unchecked(size, 0);
+        allocator.dealloc(p1.unwrap(), layout_d);
 
-#[test]
-fn usable_size_0() {
-    // This will change in the future but for now just test that it returns the size
-    // we asked for
-    let mut heap = [0; HEAP_SIZE];
-    let mut allocator = Allocator::new(&mut heap, HEAP_SIZE);
-
-    let size = 100;
-    let align = 0;
-
-    let _ = allocator.alloc(size, 0);
-    assert!(allocator.usable_size(size, align) == size);
-}
-
-#[test]
-fn realloc_0() {
-    // Should be able to allocate one thing, then reallocate it an unlimited number of times
-    let mut heap = [0; HEAP_SIZE];
-    let mut allocator = Allocator::new(&mut heap, HEAP_SIZE);
-
-    let size = 100;
-    let mut ptr = allocator.alloc(size, 0).unwrap();
-
-    for _ in 1..500 {
-        let new_ptr = allocator.realloc(ptr, size, size, 0).unwrap();
-        ptr = new_ptr;
+        // Try to reallocate the same memory freed from p1
+        let layout_a3 = Layout::from_size_align_unchecked(size, 0);
+        let p3 = allocator.alloc(layout_a3);
+        assert!(p3.is_ok());
     }
 }
 
-#[test]
-#[should_panic(expected = "Out Of Memory")]
-fn realloc_1() {
-    // This test should currently panic because the memory will become fragmented into lots of
-    // small blocks. When mereging is implemented this should be fixed.
-    let mut heap = [0; HEAP_SIZE];
-    let mut allocator = Allocator::new(&mut heap, HEAP_SIZE);
-
-    let size = 100;
-    let mut ptr = allocator.alloc(size, 0).unwrap();
-
-    for i in 1..500 {
-        let new_ptr = allocator.realloc(ptr, size + i - 1, size + i, 0).unwrap();
-        ptr = new_ptr;
-    }
-}
-
-#[test]
-fn realloc_inplace_0() {
-    // For now this do nothing...
-    let mut heap = [0; HEAP_SIZE];
-    let mut allocator = Allocator::new(&mut heap, HEAP_SIZE);
-
-    let size = 10;
-    let ptr = allocator.alloc(size, 0).unwrap();
-    assert!(allocator.realloc_inplace(ptr, size, 100, 0) == size);
-}
+// #[test]
+// fn usable_size_0() {
+//     // This will change in the future but for now just test that it returns the size
+//     // we asked for
+//     let mut heap = [0; HEAP_SIZE];
+//     let mut allocator = Allocator::new(&mut heap, HEAP_SIZE);
+//
+//     let size = 100;
+//     let align = 0;
+//
+//     let _ = allocator.alloc(size, 0);
+//     assert!(allocator.usable_size(size, align) == size);
+// }
+//
+// #[test]
+// fn realloc_0() {
+//     // Should be able to allocate one thing, then reallocate it an unlimited number of times
+//     let mut heap = [0; HEAP_SIZE];
+//     let mut allocator = Allocator::new(&mut heap, HEAP_SIZE);
+//
+//     let size = 100;
+//     let mut ptr = allocator.alloc(size, 0).unwrap();
+//
+//     for _ in 1..500 {
+//         let new_ptr = allocator.realloc(ptr, size, size, 0).unwrap();
+//         ptr = new_ptr;
+//     }
+// }
+//
+// #[test]
+// #[should_panic(expected = "Out Of Memory")]
+// fn realloc_1() {
+//     // This test should currently panic because the memory will become fragmented into lots of
+//     // small blocks. When mereging is implemented this should be fixed.
+//     let mut heap = [0; HEAP_SIZE];
+//     let mut allocator = Allocator::new(&mut heap, HEAP_SIZE);
+//
+//     let size = 100;
+//     let mut ptr = allocator.alloc(size, 0).unwrap();
+//
+//     for i in 1..500 {
+//         let new_ptr = allocator.realloc(ptr, size + i - 1, size + i, 0).unwrap();
+//         ptr = new_ptr;
+//     }
+// }
+//
+// #[test]
+// fn realloc_inplace_0() {
+//     // For now this do nothing...
+//     let mut heap = [0; HEAP_SIZE];
+//     let mut allocator = Allocator::new(&mut heap, HEAP_SIZE);
+//
+//     let size = 10;
+//     let ptr = allocator.alloc(size, 0).unwrap();
+//     assert!(allocator.realloc_inplace(ptr, size, 100, 0) == size);
+// }
