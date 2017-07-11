45eed2bf7a5c79d29e18339071c7e2f0c6851126
Jem Tucker <jem.tucker@gmail.com>
2017-07-11 17:09:35 +0100

    Cleanup commented out tests that are no longer relevant

--- libs/alloc_opsys/src/test.rs
+++ libs/alloc_opsys/src/test.rs
@@ -120,59 +120,19 @@ fn dealloc_1() {
     }
 }
 
-// #[test]
-// fn usable_size_0() {
-//     // This will change in the future but for now just test that it returns the size
-//     // we asked for
-//     let mut heap = [0; HEAP_SIZE];
-//     let mut allocator = Allocator::new(&mut heap, HEAP_SIZE);
-//
-//     let size = 100;
-//     let align = 0;
-//
-//     let _ = allocator.alloc(size, 0);
-//     assert!(allocator.usable_size(size, align) == size);
-// }
-//
-// #[test]
-// fn realloc_0() {
-//     // Should be able to allocate one thing, then reallocate it an unlimited number of times
-//     let mut heap = [0; HEAP_SIZE];
-//     let mut allocator = Allocator::new(&mut heap, HEAP_SIZE);
-//
-//     let size = 100;
-//     let mut ptr = allocator.alloc(size, 0).unwrap();
-//
-//     for _ in 1..500 {
-//         let new_ptr = allocator.realloc(ptr, size, size, 0).unwrap();
-//         ptr = new_ptr;
-//     }
-// }
-//
-// #[test]
-// #[should_panic(expected = "Out Of Memory")]
-// fn realloc_1() {
-//     // This test should currently panic because the memory will become fragmented into lots of
-//     // small blocks. When mereging is implemented this should be fixed.
-//     let mut heap = [0; HEAP_SIZE];
-//     let mut allocator = Allocator::new(&mut heap, HEAP_SIZE);
-//
-//     let size = 100;
-//     let mut ptr = allocator.alloc(size, 0).unwrap();
-//
-//     for i in 1..500 {
-//         let new_ptr = allocator.realloc(ptr, size + i - 1, size + i, 0).unwrap();
-//         ptr = new_ptr;
-//     }
-// }
-//
-// #[test]
-// fn realloc_inplace_0() {
-//     // For now this do nothing...
-//     let mut heap = [0; HEAP_SIZE];
-//     let mut allocator = Allocator::new(&mut heap, HEAP_SIZE);
-//
-//     let size = 10;
-//     let ptr = allocator.alloc(size, 0).unwrap();
-//     assert!(allocator.realloc_inplace(ptr, size, 100, 0) == size);
-// }
+#[test]
+fn usable_size_0() {
+    // This will change in the future but for now just test that it returns the size
+    // we asked for
+    let mut heap = [0; HEAP_SIZE];
+
+    unsafe {
+        let mut allocator = Allocator::new(&mut heap, HEAP_SIZE);
+
+        let size = 100;
+        let align = 0;
+
+        let _ = allocator.alloc(size, 0);
+        assert!(allocator.usable_size(size, align) == size);
+    }
+}
